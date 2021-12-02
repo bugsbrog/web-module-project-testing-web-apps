b9f5aea3f96fbd7ab8a503b1c5baf0a4581ee06f
bugsbrog <bugsbrog@gmail.com>
2021-12-02 10:54:26 -0700

    Finished 'renders without errors' test

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,8 +5,8 @@ import userEvent from '@testing-library/user-event';
 
 import ContactForm from './ContactForm';
 
-test('renders without errors', ()=>{
-    
+test('renders without errors', () => {
+    render(<ContactForm/>);
 });
 
 test('renders the contact form header', ()=> {
